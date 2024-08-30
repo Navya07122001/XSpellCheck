1409bc0e294ff2e750e3b0ff05005c79c834bc31
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-30 23:01:41 +0530

    Implemented spell check

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SpellCheckSection from './Components/SpellCheckSection';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheckSection/>
+    </div>
+  );
+}
+
+export default App;
